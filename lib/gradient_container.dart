219e70ec5c3c80f4ca5a6b67a16186edc0e35199
amo95 <jameaamo@gmail.com>
2024-02-18 15:01:25 +0000

    added start screen widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
-
-  const GradientContainer.purple({super.key})
-      : firstColor = Colors.deepPurple,
-        secondColor = Colors.indigo;
-
-  final Color firstColor;
-  final Color secondColor;
+  const GradientContainer({super.key});
 
   @override
   Widget build(context) => Container(
-
-  );
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color.fromARGB(255, 85, 46, 156),
+                Color.fromARGB(255, 57, 24, 118)
+              ], 
+              begin: Alignment.topRight, 
+              end: Alignment.bottomLeft
+              ),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset("assets/images/quiz-logo.png", width: 300),
+                  const SizedBox(height: 40),
+                  const Text(
+                    "Learn flutter the fun way!",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Start Quiz",
+                      style: TextStyle(
+                        color: Colors.white
+                        ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:quiz_app/start_screen.dart";
+
+// void main() => runApp(
+//       const MaterialApp(
+//         home: Scaffold(
+//           body: GradientContainer()
+//         ),
+//       ),
+//     );
 
 void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Color.fromARGB(255, 53, 33, 155),
-                Color.fromARGB(255, 57, 71, 152)
-              ], begin: Alignment.topRight, end: Alignment.bottomLeft),
-            ),
-            child: const Center(
-              child: Text(
-                "Hello World!",
-                style: TextStyle(color: Colors.white, fontSize: 30),
-              ),
-            ),
-          ),
+  MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: 
+          [Color.fromARGB(255, 95, 43, 145), Color.fromARGB(255, 63, 25, 140)],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft)
         ),
-      ),
-    );
+        child: const StartScreen()
+        )
+    ),
+  )
+);
